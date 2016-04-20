25ec3b490d6a1f0d7548bf45d8323dfdc6117700
youngho kim <jgj1018@gmail.com>
2016-04-20 17:29:45 +0900

    서브노트SubNote VO 추가

---
+++ src/main/java/org/javatree/www/VO/Subnote.java
@@ -0,0 +1,56 @@
+package org.javatree.www.VO;
+
+public class Subnote {
+	private int id;
+	private int subnoteno;
+	private String originalfilename;
+	private String uploadedfilename;
+	private int lectureno;
+	public Subnote(int id, int subnoteno, String originalfilename, String uploadedfilename, int lectureno) {
+		super();
+		this.id = id;
+		this.subnoteno = subnoteno;
+		this.originalfilename = originalfilename;
+		this.uploadedfilename = uploadedfilename;
+		this.lectureno = lectureno;
+	}
+	public Subnote() {
+		super();
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getSubnoteno() {
+		return subnoteno;
+	}
+	public void setSubnoteno(int subnoteno) {
+		this.subnoteno = subnoteno;
+	}
+	public String getOriginalfilename() {
+		return originalfilename;
+	}
+	public void setOriginalfilename(String originalfilename) {
+		this.originalfilename = originalfilename;
+	}
+	public String getUploadedfilename() {
+		return uploadedfilename;
+	}
+	public void setUploadedfilename(String uploadedfilename) {
+		this.uploadedfilename = uploadedfilename;
+	}
+	public int getLectureno() {
+		return lectureno;
+	}
+	public void setLectureno(int lectureno) {
+		this.lectureno = lectureno;
+	}
+	@Override
+	public String toString() {
+		return "Subnote [id=" + id + ", subnoteno=" + subnoteno + ", originalfilename=" + originalfilename
+				+ ", uploadedfilename=" + uploadedfilename + ", lectureno=" + lectureno + "]";
+	}
+	
+}
